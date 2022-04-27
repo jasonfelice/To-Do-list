7f2cf10db405cab54e5e44df88c2e1eb248e1bfb
Jason Felice <jasonfelice0@gmail.com>
2022-04-27 04:27:20 -0700

    Add linters

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
 import './styles/style.css';
+
 const listWrapper = document.querySelector('.to-do-list');
 
-let taskArray = [
-    {
-        description: 'Finish this project',
-        completed: false,
-        index: 1
-    },
-    {
-        description: 'Complete the Ableton midi project',
-        completed: false,
-        index: 2
-    },
+const taskArray = [
+  {
+    description: 'Finish this project',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Complete the Ableton midi project',
+    completed: false,
+    index: 2,
+  },
 ];
 
-
-let addTask = () => {
-  for (let each of taskArray) {
+const addTask = () => {
+  taskArray.forEach((each) => {
     const taskWrapper = document.createElement('div');
     taskWrapper.classList.add('list-item');
     const task = document.createElement('div');
     task.classList.add('task');
     const checkbox = document.createElement('input');
     checkbox.setAttribute('type', 'checkbox');
-    if(each.completed) {
+    if (each.completed) {
       checkbox.setAttribute('checked', '');
     }
     const description = document.createElement('p');
@@ -34,8 +34,7 @@ let addTask = () => {
     taskWrapper.appendChild(task);
     taskWrapper.appendChild(menuBtn);
     listWrapper.appendChild(taskWrapper);
-    console.log(task.description)
-  }
-}
+  });
+};
 
 addTask();
