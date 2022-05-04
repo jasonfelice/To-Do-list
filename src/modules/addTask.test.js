cfa77af96cf0d7820066791cede485c7fb252dec
Jason Felice <jasonfelice0@gmail.com>
2022-05-04 08:36:38 -0700

    Correct class name in test

--- src/modules/addTask.test.js
+++ src/modules/addTask.test.js
@@ -1,12 +1,14 @@
 const addTask = require('./addTask');
 
 describe('Add/Delete items from to-do list', () => {
-  test('Add one new item to the list', () => {
-    document.body.innerHTML =
-    '<div class="to-do-list">' +
-    '</div>';
-    addTask('Test', 1, false);
-    const list = document.querySelectorAll('.to-do-list');
-    expect(list).toHaveLength(1);
-  });
+    describe('Add Tasks', () => {
+        test('Add one new item to the list', () => {
+            document.body.innerHTML =
+            '<div class="to-do-list"></div>'
+            addTask('Test', 1, false);
+            addTask('Second Test', 1, false);
+            const list = document.querySelectorAll('.list-item');
+            expect(list).toHaveLength(2);
+          });
+    });
 });
