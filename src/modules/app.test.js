c24c947827a9706bb263b6bec40d9ee9ea3a9cbe
Jason Felice <jasonfelice0@gmail.com>
2022-05-05 02:20:47 -0700

    Add test for clearing completed tasks function

--- src/modules/app.test.js
+++ src/modules/app.test.js
@@ -1,4 +1,5 @@
-import addTask from './addTask';
+import addTask from './addTask.js';
+import List from './List.js';
 
 describe('Add/Delete items from to-do list', () => {
     describe('Add/Delete Tasks', () => {
@@ -16,10 +17,10 @@ describe('Add/Delete items from to-do list', () => {
             const createTaskInput = document.querySelector('#create-task');
             const clearButton = document.getElementById('clearCompleted')
             addTask('Test', 1, false);
-            addTask('Second Test', 1, false);
+            addTask('Second Test', 2, false);
             const list = document.querySelectorAll('.list-item');
             expect(list).toHaveLength(2);
-          });
+        });
         test('Delete item from the list', () => {
             document.body.innerHTML = `
             <div class="to-do-list"></div>
@@ -34,10 +35,39 @@ describe('Add/Delete items from to-do list', () => {
             const createTaskInput = document.querySelector('#create-task');
             const clearButton = document.getElementById('clearCompleted')
             addTask('Test', 1, false);
-            addTask('Second Test', 1, false);
+            addTask('Second Test', 2, false);
             document.querySelector('.del-btn').click();
             const list = document.querySelectorAll('.list-item');
             expect(list).toHaveLength(1);
-          });
+        });
+    });
+    describe('Testing edit, update and clear', () => {
+        test('Testing clear completed', () => {
+            document.body.innerHTML = `
+            <div class="to-do-list"></div>
+            <i class="return-button">
+            <input id="create-task" type="text">
+            <a href="#" id="clearCompleted"></a>
+            `;
+
+            require('./app.js')
+
+            const returnBtn = document.querySelector('.return-button');
+            const createTaskInput = document.querySelector('#create-task');
+            const clearButton = document.getElementById('clearCompleted')
+            addTask('Piano lessons', 1, true);
+            addTask('Clean the room', 2, false);
+            addTask('Laundry', 3, true);
+            addTask('Go shopping', 4, true);
+
+            const list = document.querySelectorAll('.list-item');
+            list.forEach((item) => {
+                if (item.classList.value.includes('completed')) {
+                    item.remove();
+                }
+            });
+            const Newlist = document.querySelectorAll('.list-item')
+            expect(Newlist).toHaveLength(1);
+        });
     });
 });
