9747d55af943774b5fa16a5483d1381e9d995a86
Simba Wangulu <simbawangulu@gmail.com>
2022-05-05 14:09:44 +0300

    Testing edit and update status

--- src/modules/app.test.js
+++ src/modules/app.test.js
@@ -41,5 +41,42 @@ describe('Add/Delete items from to-do list', () => {
             expect(list).toHaveLength(1);
         });
     });
-
+    describe('Testing edit, update and clear', () => {
+      test('Testing clear completed', () => {
+          document.body.innerHTML = `
+          <div class="to-do-list"></div>
+          <i class="return-button">
+          <input id="create-task" type="text">
+          <a href="#" id="clearCompleted"></a>
+          `;
+          require('./app.js')
+          const returnBtn = document.querySelector('.return-button');
+          const createTaskInput = document.querySelector('#create-task');
+          const clearButton = document.getElementById('clearCompleted')
+          addTask('Piano lessons', 1, true);
+          addTask('Clean the room', 2, false);
+          addTask('Laundry', 3, true);
+          addTask('Go shopping', 4, true);
+          const list = document.querySelectorAll('.list-item');
+          list.forEach((item) => {
+              if (item.classList.value.includes('completed')) {
+                  item.remove();
+              }
+          });
+          const Newlist = document.querySelectorAll('.list-item')
+          expect(Newlist).toHaveLength(1);
+      });
+      test('Changing completed status', () => {
+          require('./app.js')
+          List.taskList.push(new List('Walk the dogs', 1, false));
+          List.taskList[0].toggleCompleted();
+          expect(List.taskList[0].isCompleted).toBe(true);
+      });
+      test('Update task description', () => {
+          require('./app.js')
+          List.taskList.push(new List('Go shopping', 1, false));
+          List.taskList[0].updateTask('Go for a walk')
+          expect(List.taskList[0].task).toBe('Go for a walk');
+      });
+  });
 });
