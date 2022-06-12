7053c26a2b52711400107415d53066f7100c6cfc
Jake Felice <jasonfelice0@gmail.com>
2022-06-12 03:26:53 -0700

    Build

--- webpack.config.js
+++ webpack.config.js
@@ -7,14 +7,14 @@ module.exports = {
     bundle: path.resolve(__dirname, 'src/index.js'),
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     filename: '[name][contenthash].js',
     clean: true,
   },
   devtool: 'source-map',
   devServer: {
     static: {
-      directory: path.resolve(__dirname, 'dist'),
+      directory: path.resolve(__dirname, 'docs'),
     },
     port: 1700,
     hot: true,
